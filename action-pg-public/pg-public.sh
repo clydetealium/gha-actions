eb5a1b501cb3799ffcef484b37f64797b8beadc1
Clyde Tedrick <clyde.tedrick@tealium.com>
2023-08-02 16:16:17 -0400

    validate

--- action-pg-public/pg-public.sh
+++ action-pg-public/pg-public.sh
@@ -1,14 +1,18 @@
 #!/bin/bash
 FILE_MATCH='liquibase.properties'
-UPDATE_MATCH='defaultSchemaName'
+UPDATE_MATCH='defaultSchemaName:public'
 
-# Use git diff to get the changes and filter the filenames containing 'defaultSchemaName'
-changed_files=$(git diff --name-only --diff-filter=ACMRTUXB -- "${FILE_MATCH}" | xargs grep -l 'defaultSchemaName')
+# Use git diff to get the changes and filter the filenames containing 'defaultSchemaName:public'
+changed_files=$(git diff --name-only --diff-filter=ACMRTUXB -- "${FILE_MATCH}" | xargs grep -l "${UPDATE_MATCH}")
 
 # Iterate through the changed files and find the lines with the specific string
 for file in $changed_files; do
-    echo "Changes in $file:"
-    git diff --unified=0 -- $file | grep -n "${UPDATE_MATCH}" | awk -F ':' '{print $1 ":" $3}'
+    echo "${UPDATE_MATCH} found in $file:"
+    git diff --unified=0 -- $file | grep -n "${UPDATE_MATCH}"
 done
 
-echo 'fini'
+if [ -n "$changed_files" ]; then
+    echo "The above files contain the string ${UPDATE_MATCH}."
+    echo "The use of 'public' as the default schema is not allowed."
+    exit 1
+fi
