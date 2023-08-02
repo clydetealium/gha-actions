3fa23ff0b2d4d28fb2ae2c52f67fc01565081d96
Clyde Tedrick <clyde.tedrick@tealium.com>
2023-08-02 15:40:17 -0400

    hj

--- action-pg-public/pg-public.sh
+++ action-pg-public/pg-public.sh
@@ -10,6 +10,3 @@ for file in $changed_files; do
     echo "Changes in $file:"
     git diff --unified=0 -- $file | grep -n "${UPDATE_MATCH}" | awk -F ':' '{print $1 ":" $3}'
 done
-
-echo 'all clear'
-echo 'done'
